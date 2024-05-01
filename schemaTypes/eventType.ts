3134ebf60f38e6e6fe7d3b8e11424607a845f8e1
G <4167516+GytenisBorusas@users.noreply.github.com>
2024-05-01 15:36:36 -0600

    pause on sanity.io project

---
+++ schemaTypes/eventType.ts
@@ -0,0 +1,53 @@
+import {defineField, defineType} from 'sanity'
+
+export const eventType = defineType({
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'name'},
+    }),
+    defineField({
+      name: 'date',
+      type: 'datetime',
+      options: {
+        list: ['in-person', 'virtual'],
+        layout: 'radio',
+      },
+    }),
+    defineField({
+      name: 'doorsOpen',
+      type: 'number',
+    }),
+    defineField({
+      name: 'venue',
+      type: 'reference',
+      to: [{type: 'venue'}],
+    }),
+    defineField({
+      name: 'headline',
+      type: 'reference',
+      to: [{type: 'artist'}],
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'details',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'tickets',
+      type: 'url',
+    }),
+  ],
+})
